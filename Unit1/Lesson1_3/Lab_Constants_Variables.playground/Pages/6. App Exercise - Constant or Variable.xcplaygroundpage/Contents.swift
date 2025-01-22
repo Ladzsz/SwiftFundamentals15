678f118d048c0493fe9e931ba3259db9130d83cc
London Mason <londonmason602@gmail.com>
2025-01-22 10:41:28 -0500

    finished the third lesson
    did all pages

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Pages/6. App Exercise - Constant or Variable.xcplaygroundpage/Contents.swift
@@ -11,8 +11,11 @@
 - Goal number of steps: The user's goal for number of steps to take each day
 - Average heart rate: The user's average heart rate over the last 24 hours
  */
-
-
+var name = "Bob"
+var age = 18
+var steps = 3000
+let stepGoal = 50000
+var heartRate = 70
 /*:
  Now go back and add a line after each constant or variable declaration. On those lines, print a statement explaining why you chose to declare the piece of information as a constant or variable.
  
